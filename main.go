82c1b5fde3bb9e75fb382ee57a87d19b5eea1b05
BoxCosplayer <rajveer@sandhuhome.uk>
2025-12-15 14:50:52 +0800

    completed

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"fmt"
 	"os"
 
@@ -8,31 +8,82 @@ import(
 )
 
 type model struct {
-	choices []string
-	cursor int
+	choices  []string
+	cursor   int
 	selected map[int]struct{}
 }
 
 func initialModel() model {
 	return model{
-		choices: []string{"Bread", "Milk", "Salt", "Sugar", "Flour"},
-		selected: (make(map[int]struct{}))
+		choices:  []string{"Bread", "Milk", "Salt", "Sugar", "Flour"},
+		selected: make(map[int]struct{}),
 	}
 }
 
-// function that takes in a model and performes a command
-func(m model) Init() tea.Cmd {
+// Init satisfies the tea.Model interface and runs on program start.
+func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func(m model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	
-	// is it a key press
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
 		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What should we buy at the market?\n\n"
+
+	for i, choice := range m.choices {
+
+		cursor := " " // init cursor
+
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("ERROR!!!! 🚨🚨🚨 ERROR!!!! 🚨🚨🚨 ERROR!!!! 🚨🚨🚨 ERROR!!!! 🚨🚨🚨 ERROR!!!! 🚨🚨🚨: %v", err)
+		os.Exit(1)
 	}
 }
